2888df11654659c92a7378d81be5b3495685a413
Queops <queops@gmail.com>
2020-12-06 22:50:02 -0300

    Primeiro commit entrega do projeto

---
+++ Models/infectadoDto.cs
@@ -0,0 +1,10 @@
+namespace Api.Models
+{
+    public class infectadoDto
+    {
+        public System.DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }  
+    }
+}
